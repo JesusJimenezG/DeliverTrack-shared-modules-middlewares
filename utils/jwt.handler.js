3ced9635f0027d90b2c01c2b32571f65a29287c7
Jesus <jesusalfonsojg7@gmail.com>
2023-03-24 12:01:12 -0400

    Rolled back folder structure

---
+++ utils/jwt.handler.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.verifyToken = exports.generateToken = void 0;
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const generateToken = (hash, jwt_secret) => {
+    const token = jsonwebtoken_1.default.sign({ fingerprint: hash }, jwt_secret);
+    return { token };
+};
+exports.generateToken = generateToken;
+const verifyToken = (token, jwt_secret) => {
+    const decoded = jsonwebtoken_1.default.verify(token, jwt_secret);
+    return decoded;
+};
+exports.verifyToken = verifyToken;
